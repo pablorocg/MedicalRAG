66b9fa65d46b3bc1e5a8dbd2fbbda6875bc65131
Pablo RG <62758137+pablorocg@users.noreply.github.com>
2024-03-20 11:19:17 +0100

    Actualizacion

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 class CFG:
     embedding_model = 'TimKond/S-PubMedBert-MedQuAD'
     n_samples = None
-    with_na = False
+    with_na = True
     batch_size = 128
     device = 'cuda'
+    log_embeddings = False
 
     assert embedding_model in ['emilyalsentzer/Bio_ClinicalBERT', 'TimKond/S-PubMedBert-MedQuAD'], 'Invalid embedding model'
